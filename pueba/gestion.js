c08dddfbca0e3eca26af8530fa55beb6f40481ab
lorca03 <guillermolorcamartinez@gmail.com>
2022-10-10 14:41:56 +0200

    reto

--- pueba/gestion.js
+++ pueba/gestion.js
@@ -23,50 +23,55 @@ class Tanque {
 const tanques = [];
 const habitantes = [];
 const localidades = [];
-const pantalla=document.getElementById("pantalla");
+const pantalla = document.getElementById("pantalla");
 
 function añadir(elemento) {
-    pantalla.style.display='block';
+  pantalla.style.display = "block";
   switch (elemento.name) {
     case "tanque":
-        document.getElementById("añadirTanque").style.display='block'; 
-        document.getElementById("añadirLocalidad").style.display='none'; 
+      document.getElementById("añadirTanque").style.display = "block";
+      document.getElementById("añadirLocalidad").style.display = "none";
+      document.getElementById("añadirHabitantes").style.display = "none";
       break;
     case "localidad":
-        document.getElementById("añadirLocalidad").style.display='block';
-        document.getElementById("añadirTanque").style.display='none';  
+      document.getElementById("añadirLocalidad").style.display = "block";
+      document.getElementById("añadirTanque").style.display = "none";
+      document.getElementById("añadirHabitantes").style.display = "none";
+      break;
+    case "habitante":
+      document.getElementById("añadirLocalidad").style.display = "none";
+      document.getElementById("añadirTanque").style.display = "none";
+      document.getElementById("añadirHabitantes").style.display = "block";
+      break;
+  }
+}
+function eliminar(elemento) {
+  switch (elemento.name) {
+    case "tanque":
+      break;
+    case "localidad":
       break;
     case "habitante":
       break;
   }
 }
-function eliminar(elemento) {
-    switch (elemento.name) {
-        case "tanque":
-          break;
-        case "localidad":
-          break;
-        case "habitante":
-          break;
-      }
-}
 function modificar(elemento) {
-    switch (elemento.name) {
-        case "tanque":
-          break;
-        case "localidad":
-          break;
-        case "habitante":
-          break;
-      }
+  switch (elemento.name) {
+    case "tanque":
+      break;
+    case "localidad":
+      break;
+    case "habitante":
+      break;
+  }
 }
 function visualizar(elemento) {
-    switch (elemento.name) {
-        case "tanque":
-          break;
-        case "localidad":
-          break;
-        case "habitante":
-          break;
-      }
+  switch (elemento.name) {
+    case "tanque":
+      break;
+    case "localidad":
+      break;
+    case "habitante":
+      break;
+  }
 }
